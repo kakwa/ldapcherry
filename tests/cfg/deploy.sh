9c52414ff7e2095b6b633957a8250ca836d033aa
kakwa <carpentier.pf@gmail.com>
2015-04-14 20:27:53 +0200

    trying to install debian samba 4 from wheezy backport

--- tests/cfg/deploy.sh
+++ tests/cfg/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 DEBIAN_FRONTEND=noninteractive apt-get install ldap-utils slapd -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
-DEBIAN_FRONTEND=noninteractive apt-get install samba python-samba -t wheezy-backports -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
+DEBIAN_FRONTEND=noninteractive apt-get install lsb-base libattr1 -t wheezy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
+DEBIAN_FRONTEND=noninteractive apt-get install samba python-samba samba-vfs-modules -t wheezy-backports -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y
 
 rsync -a `dirname $0`/ /
 cd `dirname $0`/../../
