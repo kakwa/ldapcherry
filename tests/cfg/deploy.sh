8603f3f88481392a76d10d27cddf77dd7bfb37ca
pcarpent <pcarpent@grant.idsi0.si.c-s.fr>
2015-04-14 16:04:33 +0200

    deployement of ldap

--- tests/cfg/deploy.sh
+++ tests/cfg/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo rsync -a `dirname $0`/ /
+rsync -a `dirname $0`/ /
 cd `dirname $0`/../../
 sudo sed -i "s%template_dir.*%template_dir = '`pwd`/resources/templates/'%" /etc/ldapcherry/ldapcherry.ini
 sudo sed -i "s%tools.staticdir.dir.*%tools.staticdir.dir = '`pwd`/resources/static/'%" /etc/ldapcherry/ldapcherry.ini
 
-sudo chown -R openldap:openldap /etc/ldap/
-sudo /etc/init.d/slapd restart
+chown -R openldap:openldap /etc/ldap/
+/etc/init.d/slapd restart
 ldapadd -H ldap://localhost -x -D "cn=admin,dc=example,dc=org" -f /etc/ldap/content.ldif -w password
 sed -i "s/\(127.0.0.1.*\)/\1 ldap.ldapcherry.org/" /etc/hosts
