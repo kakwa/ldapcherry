241ecb5ff707601aef32ae06701d0287a9e5d206
root <root@tsingtao.kakwa.fr>
2015-04-14 21:09:01 +0200

    various fixes un deploy.sh

--- tests/cfg/deploy.sh
+++ tests/cfg/deploy.sh
@@ -15,22 +15,26 @@ sudo sed -i "s%template_dir.*%template_dir = '`pwd`/resources/templates/'%" /etc
 sudo sed -i "s%tools.staticdir.dir.*%tools.staticdir.dir = '`pwd`/resources/static/'%" /etc/ldapcherry/ldapcherry.ini
 
 chown -R openldap:openldap /etc/ldap/
+rm /etc/ldap/slapd.d/cn\=config/*mdb*
 /etc/init.d/slapd restart
-ldapadd -H ldap://localhost -x -D "cn=admin,dc=example,dc=org" -f /etc/ldap/content.ldif -w password
+ldapadd -H ldap://localhost:390  -x -D "cn=admin,dc=example,dc=org" -f /etc/ldap/content.ldif -w password
 sed -i "s/\(127.0.0.1.*\)/\1 ldap.ldapcherry.org ad.ldapcherry.org/" /etc/hosts
 
 
 smbconffile=/etc/samba/smb.conf
 domain=dc
 realm=dc.ldapcherry.org
-sambadns=INTERNAL
+sambadns=SAMBA_INTERNAL
 targetdir=/var/lib/samba/
 role=dc
+sambacmd=samba-tool
+adpass=qwertyP455
+
 printf '' > "${smbconffile}" && \
     ${sambacmd} domain provision ${hostip} \
     --domain="${domain}" --realm="${realm}" --dns-backend="${sambadns}" \
     --targetdir="${targetdir}" --workgroup="${domain}" --use-rfc2307 \
-    --configfile="${smbconffile}" --server-role="${role}" -d 1 && \
+    --configfile="${smbconffile}" --server-role="${role}" -d 1 --adminpass="${adpass}" && \
     mv "${targetdir}/etc/smb.conf" "${smbconffile}"
 
 /etc/init.d/samba-ad-dc restart
