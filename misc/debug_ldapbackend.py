b65fe7bc7b43a9aeee1319581392fa2ffad9f2e1
kakwa <carpentier.pf@gmail.com>
2015-05-22 20:07:09 +0200

    adding small example for backend ldap

---
+++ misc/debug_ldapbackend.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+from __future__ import unicode_literals
+
+import pytest
+import sys
+from sets import Set
+from ldapcherry.backend.backendLdap import Backend
+from ldapcherry import syslog_error
+from ldapcherry.exceptions import *
+import cherrypy
+from ldap import SERVER_DOWN
+
+cfg = {
+'module'            : 'ldapcherry.backend.ldap',
+'groupdn'           : 'ou=group,dc=example,dc=org',
+'userdn'            : 'ou=People,dc=example,dc=org',
+'binddn'            : 'cn=dnscherry,dc=example,dc=org',
+'password'          : 'password',
+'uri'               : 'ldap://ldap.ldapcherry.org:390',
+'ca'                : './tests/test_env/etc/ldapcherry/TEST-cacert.pem',
+'starttls'          : 'off',
+'checkcert'         : 'off',
+'user_filter_tmpl'  : '(uid=%(username)s)',
+'group_filter_tmpl' : '(member=%(userdn)s)',
+}
+
+cherrypy.log.error = syslog_error
+attr = ['shéll', 'cn', 'uid', 'uidNumber', 'gidNumber', 'home', 'userPassword', 'givenName', 'email', 'sn']
+
+inv = Backend(cfg, cherrypy.log, 'ldap', attr)
+print inv.get_user('jwatson')
