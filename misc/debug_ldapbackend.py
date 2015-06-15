f79121e1e5cbbcd076c1165da9f897fe9ed7d0cf
kakwa <carpentier.pf@gmail.com>
2015-06-15 07:31:44 +0200

    fix misc example for ldap backend usage

--- misc/debug_ldapbackend.py
+++ misc/debug_ldapbackend.py
@@ -15,20 +15,23 @@ import logging
 from ldap import SERVER_DOWN
 
 cfg = {
-'module'             : 'ldapcherry.backend.ldap',
-'groupdn'            : 'ou=Groups,dc=example,dc=org',
-'userdn'             : 'ou=People,dc=example,dc=org',
-'binddn'             : 'cn=dnscherry,dc=example,dc=org',
-'password'           : 'password',
-'uri'                : 'ldap://ldap.ldapcherry.org:390',
-'ca'                 : './tests/test_env/etc/ldapcherry/TEST-cacert.pem',
-'starttls'           : 'off',
-'checkcert'          : 'off',
-'user_filter_tmpl'   : '(uid=%(username)s)',
-'group_filter_tmpl'  : '(member=%(userdn)s)',
-'search_filter_tmpl' : '(|(uid=%(searchstring)s*)(sn=%(searchstring)s*))',
-'objectclasses'      : 'top, person, organizationalPerson, simpleSecurityObject, posixAccount',
-'dn_user_attr'       : 'uid',
+'module'                : 'ldapcherry.backend.ldap',
+'groupdn'               : 'ou=Groups,dc=example,dc=org',
+'userdn'                : 'ou=People,dc=example,dc=org',
+'binddn'                : 'cn=dnscherry,dc=example,dc=org',
+'password'              : 'password',
+'uri'                   : 'ldap://ldap.ldapcherry.org:390',
+'ca'                    : './tests/test_env/etc/ldapcherry/TEST-cacert.pem',
+'starttls'              : 'off',
+'checkcert'             : 'off',
+'user_filter_tmpl'      : '(uid=%(username)s)',
+'group_filter_tmpl'     : '(member=%(userdn)s)',
+'search_filter_tmpl'    : '(|(uid=%(searchstring)s*)(sn=%(searchstring)s*))',
+'objectclasses'         : 'top, person, organizationalPerson, simpleSecurityObject, posixAccount',
+'dn_user_attr'          : 'uid',
+'group_attr.uniqMember' : "%(dn)s",
+'group_attr.memberUid'  : "%(uid)s",
+
 }
 
 def syslog_error(msg='', context='',
@@ -40,7 +43,7 @@ attr = ['shéll', 'cn', 'uid', 'uidNumber', 'gidNumber', 'home', 'userPassword',
 
 cherrypy.log.error = syslog_error
 
-inv = Backend(cfg, cherrypy.log, 'ldap', attr)
+inv = Backend(cfg, cherrypy.log, 'ldap', attr, 'uid')
 print inv.get_user('jwatson')
 print inv.get_groups('jwatson')
 print inv.search('smit')
@@ -57,3 +60,6 @@ inv.add_user(user)
 print inv.get_user('test')
 print inv.get_groups('test')
 inv.del_user('test')
+
+
+print inv.group_attrs
