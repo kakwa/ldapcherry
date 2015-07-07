2d06e14d63c093a9e33686cac62d29fa640a90cc
kakwa <carpentier.pf@gmail.com>
2015-07-08 00:42:53 +0200

    adding skeleton of autofill functions

---
+++ resources/static/js/lc-filler.js
@@ -0,0 +1,28 @@
+/* 
+ * Licensed under the MIT public license.
+ *
+ * Part of LdapCherry.
+ *
+ * Functions to autofill form fields from other fields.
+ *
+*/
+
+function lcUid(firstname, lastname){
+    return 'test';
+}
+
+function lcDisplayName(firstname, lastname){
+    return 'test';
+}
+
+function lcMail(firstname, lastname, domain){
+    return 'test@test';
+}
+
+function lcUidNumber(firstname, lastname){
+    return 42;
+}
+
+function lcHomeDir(firstname, lastname){
+    return '/home/test';
+}
