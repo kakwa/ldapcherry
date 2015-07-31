76d85339916472181a0e5b2e5efa006856d09f51
kakwa <carpentier.pf@gmail.com>
2015-07-31 20:30:04 +0200

    pretier and cleaner run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+Red='\33[0;31m';
+Gre='\33[0;32m';
+RCol='\33[0m';
+
 cd `dirname $0`
 python setup.py test &&\
-pep8 --repeat --show-source --exclude=.venv,.tox,dist,docs,build,*.egg,tests,misc .
+printf "\nPEP 8 compliance check:\n\n"
+pep8 \
+    --repeat \
+    --show-source \
+    --exclude=.venv,.tox,dist,docs,build,*.egg,tests,misc . && \
+    printf "[${Gre}Passed${RCol}] Yeah! everything is clean\n\n" || \
+    printf "[${Red}Failed${RCol}] Oh No! there is some mess to fix\n\n"
