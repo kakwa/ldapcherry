e45c0e862e3669af36a5962ca812c3074562033a
kakwa <carpentier.pf@gmail.com>
2016-07-05 19:57:35 +0200

    reindent javascript

--- resources/static/js/ppolicy.js
+++ resources/static/js/ppolicy.js
@@ -1,24 +1,26 @@
 $('#form').validator({
     custom: {
         'ppolicy': function($el) {
-                if(! $el.prop('required') && $el.val() == 0){
-                    return true;
-                };
-                var $ret = 'PPolicy error';
-                $.ajax({
-                  url: '/checkppolicy',
-                  dataType: 'json',
-                  async: false,
-                  data: 'pwd=' + $el.val(),
-                  success: function(data) {
+            if(! $el.prop('required') && $el.val() == 0){
+                return true;
+            };
+            var $ret = 'PPolicy error';
+            $.ajax({
+                url: '/checkppolicy',
+                dataType: 'json',
+                async: false,
+                data: 'pwd=' + $el.val(),
+                success: function(data) {
                     $ret = data;
-                  }
-                });
-                this.options.errors['ppolicy'] = $ret['reason'];
-                return $ret['match'];
-	}
+                }
+            });
+            this.options.errors['ppolicy'] = $ret['reason'];
+            return $ret['match'];
+        }
     },
     errors: {
         'ppolicy': 'PPolicy error',
     }
 })
+
+// vim:set expandtab tabstop=4 shiftwidth=4:
