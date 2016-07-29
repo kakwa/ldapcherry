52557afa6a58d8c7e186ccedc2e45d1cc41396e4
kakwa <carpentier.pf@gmail.com>
2016-07-29 07:34:58 +0200

    adding a decorator to disable slow tests
    if env var LCNOSLOW=yes, the slowest tests are disable

--- tests/disable.py
+++ tests/disable.py
@@ -6,3 +6,11 @@ def travis_disabled(f):
         return _decorator
     else:
         return f
+
+def slow_disabled(f):
+    def _decorator(f):
+            print 'test has been disabled by env var LCNOSLOW'
+    if 'LCNOSLOW' in os.environ and os.environ['LCNOSLOW'] == 'yes':
+        return _decorator
+    else:
+        return f
