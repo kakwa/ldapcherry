a404cf0b395d013f45a4143d2005947a7ae8987c
kakwa <carpentier.pf@gmail.com>
2017-04-06 20:57:42 +0200

    add auto message for tagging script

--- goodies/tag.sh
+++ goodies/tag.sh
@@ -4,5 +4,5 @@ cd  `dirname $0`/../
 
 version=`sed -e "s/version\ * = \ *'\(.*\)'.*/\1/;tx;d;:x" ./ldapcherry/version.py`
 
-git tag "$version"
+git tag "$version" -m "version $version"
 git push origin "$version"
