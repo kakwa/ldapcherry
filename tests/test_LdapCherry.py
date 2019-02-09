e50df5dde31ea97b2217b84aff8288ea186612f4
kakwa <carpentier.pf@gmail.com>
2019-02-09 19:26:31 +0100

    wider exception for <input> attribute "type" has invalid value

--- tests/test_LdapCherry.py
+++ tests/test_LdapCherry.py
@@ -95,7 +95,7 @@ def _is_html_error(line):
                 r'.*Warning: <form> proprietary attribute.*',
                 r'.*Warning: <table> lacks "summary" attribute.*',
                 r'.*Warning: <script> inserting "type" attribute.*',
-                r'.*Warning: <input> attribute "id" has invalid value "#password2".*',
+                r'.*Warning: <input> attribute "id" has invalid value.*',
              ]:
         if re.match(p, line):
             return False
